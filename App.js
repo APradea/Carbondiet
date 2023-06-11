529ad765089b05fc7fe101f5817310f852e38793
Anthelme Pradeau <anthelme.p@gmail.com>
2023-06-11 22:25:27 +0200

    test

---
+++ App.js
@@ -0,0 +1,38 @@
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+
+export default function App() {
+  const handlePress = () => console.log("Hello!");
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="auto" />
+      <TouchableOpacity onPress={handlePress}>
+        <Image
+          blurRadius={5}
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "dodgerblue",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
